9309737c238529cd8d355a0a7bc7c384279d2358
Carolina-mv80 <carolinamendoza.instructor@gmail.com>
2024-06-22 08:00:34 -0500

    agregar ejercicio 1

---
+++ ejercicio1 - Cap mostrar texto.py
@@ -0,0 +1,10 @@
+
+print("FORMA 1")
+print("¿Digite un texto o palabra?")
+nombre = input()
+print ("el texto digitado es: ", nombre)
+
+print("FORMA 2")
+nombre = input("Digite un texto o palabra")
+print ("el texto digitado es: ", nombre)
+
